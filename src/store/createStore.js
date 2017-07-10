4209cc61b23562c97124816057778e28ef55b61e
F. Javier R. Donado <fjrd84@gmail.com>
2017-07-11 00:00:50 +0200

    Remove fbConfig.

--- src/store/createStore.js
+++ src/store/createStore.js
@@ -5,15 +5,6 @@ import makeRootReducer from './reducers'
 import { updateLocation } from './location'
 // import { reactReduxFirebase } from 'react-redux-firebase'
 
-const fbConfig = {
-  apiKey: 'AIzaSyBqxNTm3NLPmDs2rcXEean4sVlaxgV2OoU',
-  authDomain: 'health-nlp-88b08.firebaseapp.com',
-  databaseURL: 'https://health-nlp-88b08.firebaseio.com',
-  projectId: 'health-nlp-88b08',
-  storageBucket: 'health-nlp-88b08.appspot.com',
-  messagingSenderId: '329948122061'
-}
-
 const createStore = (initialState = {}) => {
   // ======================================================
   // Middleware Configuration
