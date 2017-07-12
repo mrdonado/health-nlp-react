86d408e2c90bc708450a864f8bfeb95c65218aff
F. Javier R. Donado <fjrd84@gmail.com>
2017-07-12 21:43:28 +0200

    Add get-analysis actions.

---
+++ src/actions/get-analysis.js
@@ -0,0 +1,35 @@
+import ActionTypes from '../constants/action-types';
+import database from './firebase-db';
+
+export function getAnalysis() {
+  return dispatch => {
+    dispatch(getAnalysisRequestedAction());
+    return database.ref('/').once('value', snap => {
+      const analysis = snap.val();
+      dispatch(getAnalysisFulfilledAction(analysis))
+    })
+    .catch((error) => {
+      console.log(error);
+      dispatch(getAnalysisRejectedAction());
+    });
+  }
+}
+
+function getAnalysisRequestedAction() {
+  return {
+    type: ActionTypes.GetAnalysisRequested
+  };
+}
+
+function getAnalysisRejectedAction() {
+  return {
+    type: ActionTypes.GetAnalysisRejected
+  }
+}
+
+function getAnalysisFulfilledAction(analysis) {
+  return {
+    type: ActionTypes.GetAnalysisFulfilled,
+    analysis
+  };
+}
---
+++ src/constants/action-types.js
@@ -0,0 +1,7 @@
+const actionTypes = {
+  GetAnalysisRequested: 'GET_ANALYSIS_REQUESTED',
+  GetAnalysisRejected: 'GET_ANALYSIS_REJECTED',
+  GetAnalysisFulfilled: 'GET_ANALYSIS_FULFILLED'
+};
+
+export default actionTypes;
