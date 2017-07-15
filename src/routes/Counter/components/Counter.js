acc6d44bcf805b39a6b82bf94be19645a0a01e7d
F. Javier R. Donado <fjrd84@gmail.com>
2017-07-15 23:36:31 +0200

    Define component as a class.

--- src/routes/Counter/components/Counter.js
+++ src/routes/Counter/components/Counter.js
@@ -1,22 +1,33 @@
-import React from 'react'
-import PropTypes from 'prop-types'
+import React from 'react';
+import PropTypes from 'prop-types';
 
-export const Counter = ({ counter, increment, doubleAsync }) => (
-  <div style={{ margin: '0 auto' }} >
-    <h2>Counter: {counter}</h2>
-    <button className='btn btn-primary' onClick={increment}>
-      Increment
+class Counter extends React.Component {
+
+  componentDidMount = () => {
+    this.props.onGetAnalysis();
+  };
+  render() {
+    return <div style={{ margin: '0 auto' }} >
+      <h2>Counter: {this.props.counter}</h2>
+      <button className='btn btn-primary' onClick={this.props.increment}>
+        Increment
     </button>
-    {' '}
-    <button className='btn btn-secondary' onClick={doubleAsync}>
-      Double (Async)
+      {' '}
+      <button className='btn btn-secondary' onClick={this.props.doubleAsync}>
+        Double (Async)
     </button>
-  </div>
-)
+    </div >;
+  }
+
+}
+
+
+
 Counter.propTypes = {
   counter: PropTypes.number.isRequired,
   increment: PropTypes.func.isRequired,
   doubleAsync: PropTypes.func.isRequired,
-}
+  onGetAnalysis: PropTypes.func.isRequired
+};
 
-export default Counter
+export default Counter;
