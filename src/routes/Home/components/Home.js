05cbb824f70a92c56c8ce3cb115ca8b471cff233
F. Javier R. Donado <fjrd84@gmail.com>
2017-07-19 23:24:03 +0200

    Display analysis on Home component.

--- src/routes/Home/components/Home.js
+++ src/routes/Home/components/Home.js
@@ -1,10 +1,35 @@
 import React from 'react';
+import PropTypes from 'prop-types';
 import './Home.scss';
 
-export const Home = () => (
-  <div id='analysis-section'>
-    Home View
-  </div>
-);
+const renderAnalysis = (data) => {
+  if (typeof data.analysis === 'undefined') {
+    return '';
+  }
+  const keys = Object.keys(data.analysis);
+  let template = [];
+  keys.forEach((key) => {
+    template.push(<div>{data.analysis[key].analysis.problem}</div>);
+  });
+  return template;
+};
+
+
+class Home extends React.Component {
+
+  componentDidMount = () => {
+    this.props.onGetAnalysis();
+  };
+  render() {
+    return <div id='analysis-section'>
+      {this.props.analysis ? renderAnalysis(this.props.analysis) : 'no-analysis'}
+    </div>;
+  }
+}
+
+Home.propTypes = {
+  analysis: PropTypes.any.isRequired,
+  onGetAnalysis: PropTypes.func.isRequired
+};
 
 export default Home;
