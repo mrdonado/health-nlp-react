4b342feb526f17d528e013417e633eeeec0d2082
F. Javier R. Donado <fjrd84@gmail.com>
2017-07-19 22:06:39 +0200

    Remove data example.

--- src/routes/Home/components/HomeView.js
+++ src/routes/Home/components/HomeView.js
@@ -1,31 +1,9 @@
 import React from 'react';
 import './HomeView.scss';
 
-const items = [
-  {
-    message: 'Some message with a problem and a solution',
-    analysis: {
-      problem: 'a problem',
-      solution: 'a solution'
-    }
-  },
-  {
-    message: 'Some other message with a problem and a solution',
-    analysis: {
-      problem: 'a problem',
-      solution: 'a solution'
-    }
-  }];
-
 export const HomeView = () => (
   <div id='analysis-section'>
-    {
-      items.map((item, idx) => {
-        return <ul key={idx.toString()} className='analysis-list'>
-          {item.message}
-        </ul>;
-      })
-    }
+    Home View
   </div>
 );
 
