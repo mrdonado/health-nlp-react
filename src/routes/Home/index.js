e8e177d487167f3dfdc71c519ec322c55d165ab4
F. Javier R. Donado <fjrd84@gmail.com>
2017-07-19 23:23:15 +0200

    No path for the home route.

--- src/routes/Home/index.js
+++ src/routes/Home/index.js
@@ -7,7 +7,6 @@
 import { injectReducer } from '../../store/reducers';
 
 export default (store) => ({
-  path : 'counter',
   /*  Async getComponent is only invoked when route matches   */
   getComponent (nextState, cb) {
     /*  Webpack - use 'require.ensure' to create a split point
