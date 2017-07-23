6d433c99ff23dd8ab86b2d33b119a7cdc3331e49
F. Javier R. Donado <fjrd84@gmail.com>
2017-07-23 14:09:05 +0200

    create-react-app bootstrap due to react-redux-starter-kit's deprecation notice.

--- build/scripts/start.js
+++
@@ -1,6 +0,0 @@
-const logger = require('../lib/logger');
-
-logger.info('Starting server...');
-require('../../server/main').listen(7777, () => {
-  logger.success('Server is running at http://localhost:7777');
-});
