e7ceeb4ca9cf93801ed76bc247abaeaeaaae3871
F. Javier R. Donado <fjrd84@gmail.com>
2017-07-23 18:34:05 +0200

    Remove unused styles.

--- src/App.js
+++ src/App.js
@@ -1,13 +1,11 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.scss'
+import './App.css'
 
 class App extends Component {
   render() {
     return (
       <div className="App">
         <div className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
           <h2>Lifescope</h2>
         </div>
       </div>
