3935b2c1d7990a0f6862c3aa28fc79cf114fbaad
F. Javier R. Donado <fjrd84@gmail.com>
2017-07-24 23:34:36 +0200

    Remove redundant index.

--- src/modules/index.js
+++ src/modules/index.js
@@ -1,6 +1,6 @@
 import { combineReducers } from 'redux';
 import { routerReducer } from 'react-router-redux';
-import { analysisReducer } from './analysis/index';
+import { analysisReducer } from './analysis';
 import counter from './counter';
 
 export default combineReducers({
