da205ee0a003df51238e66f87817d9fe8763159d
F. Javier R. Donado <fjrd84@gmail.com>
2017-07-25 00:15:30 +0200

    Remove counter example from home.

--- src/containers/home/index.js
+++ src/containers/home/index.js
@@ -2,13 +2,6 @@ import React from 'react';
 import { push } from 'react-router-redux';
 import { bindActionCreators } from 'redux';
 import { connect } from 'react-redux';
-import {
-  increment,
-  incrementAsync,
-  decrement,
-  decrementAsync
-} from '../../modules/counter';
-
 import { getAnalysis } from '../../modules/analysis';
 
 const renderAnalysis = (data) => {
@@ -30,38 +23,20 @@ class Home extends React.Component {
   render() {
     return <div>
       <h1>Home</h1>
-      <p>Count: {this.props.count}</p>
       <div id='analysis-section'>
         {this.props.analysis ? renderAnalysis(this.props.analysis) : 'no-analysis'}
       </div>
-      <p>
-        <button onClick={this.props.increment} disabled={this.props.isIncrementing}>Increment</button>
-        <button onClick={this.props.incrementAsync} disabled={this.props.isIncrementing}>Increment Async</button>
-      </p>
-
-      <p>
-        <button onClick={this.props.decrement} disabled={this.props.isDecrementing}>Decrementing</button>
-        <button onClick={this.props.decrementAsync} disabled={this.props.isDecrementing}>Decrement Async</button>
-      </p>
-
       <p><button onClick={() => this.props.changePage()}>Go to about page via redux</button></p>
     </div>;
   }
 };
 
 const mapStateToProps = state => ({
-  analysis: state.analysis,
-  count: state.counter.count,
-  isIncrementing: state.counter.isIncrementing,
-  isDecrementing: state.counter.isDecrementing
+  analysis: state.analysis
 });
 
 const mapDispatchToProps = dispatch => bindActionCreators({
   getAnalysis,
-  increment,
-  incrementAsync,
-  decrement,
-  decrementAsync,
   changePage: () => push('/about-us')
 }, dispatch);
 
--- src/modules/index.js
+++ src/modules/index.js
@@ -1,10 +1,8 @@
 import { combineReducers } from 'redux';
 import { routerReducer } from 'react-router-redux';
 import { analysisReducer } from './analysis';
-import counter from './counter';
 
 export default combineReducers({
   routing: routerReducer,
-  analysis: analysisReducer,
-  counter
+  analysis: analysisReducer
 });
