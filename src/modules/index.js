5dbeca06ef9f3c1a78cec2321221b884e1681932
F. Javier R. Donado <fjrd84@gmail.com>
2017-07-24 23:32:31 +0200

    Combine the analysis reducer.

--- src/modules/index.js
+++ src/modules/index.js
@@ -1,8 +1,10 @@
 import { combineReducers } from 'redux';
 import { routerReducer } from 'react-router-redux';
+import { analysisReducer } from './analysis/index';
 import counter from './counter';
 
 export default combineReducers({
   routing: routerReducer,
+  analysis: analysisReducer,
   counter
 });
