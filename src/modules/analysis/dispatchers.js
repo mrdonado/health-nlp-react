562962d2c365efdaf782d9db4d02109313c6552b
F. Javier R. Donado <fjrd84@gmail.com>
2017-08-02 22:04:08 +0200

    Initialize the db for all the dispatchers.

--- src/modules/analysis/dispatchers.js
+++ src/modules/analysis/dispatchers.js
@@ -1,50 +1,72 @@
 import Actions from './actions';
 
 /**
- * Action dispatcher getAnalysis (default export). In order
- * to use it, it must be initialized with a firebase database
- * first (@see ./firebase/firebase-db.js).
- * 
- * This thunk retrieves the specified count of latest analysis,
- * dispatching a getAnalysisFulfilled action on success and
- * a getAnalysisRejected action on failure.
- * 
- * @param {number} count 
+ * analysisDispatchers. In order to use them, they 
+ * must be first initialized with a firebase database
+ * @see ./firebase/firebase-db.js
+ * @param {*} database 
  */
-const getAnalysis = (database) => (count = 5) => {
-  return dispatch => {
-    dispatch(getAnalysisRequestedAction());
-    return database.ref('/analysis')
-      .limitToLast(count)
-      .once('value',
-      snap => {
-        const analysis = snap.val();
-        dispatch(getAnalysisFulfilledAction(analysis));
-      })
-      .catch((error) => {
-        console.log(error);
-        dispatch(getAnalysisRejectedAction());
+const analysisDispatchers = (database) => {
+
+  /**
+  * 
+   * This thunk retrieves the specified count of latest analysis,
+   * dispatching a getResultsFulfilled action on success and
+   * a getResultsRejected action on failure.
+   * 
+   * @param {number} count 
+   */
+  const getResults = () => {
+    return (dispatch, getState) => {
+      const count = getState().analysis.resultsCount;
+      dispatch(getResultsRequestedAction());
+      return database.ref('/analysis')
+        .limitToLast(count)
+        .once('value',
+        snap => {
+          const results = snap.val();
+          dispatch(getResultsFulfilledAction(results));
+        })
+        .catch((error) => {
+          console.log(error);
+          dispatch(getResultsRejectedAction());
+        });
+    };
+  };
+
+  const getResultsRequestedAction = () => {
+    return {
+      type: Actions.GetAnalysisRequested
+    };
+  };
+
+  const getResultsRejectedAction = () => {
+    return {
+      type: Actions.GetAnalysisRejected
+    };
+  };
+
+  const getResultsFulfilledAction = (results) => {
+    return {
+      type: Actions.GetAnalysisFulfilled,
+      results
+    };
+  };
+
+  /**
+   * When requesting more results, the current number of
+   * desired results must be increased (action MoreResults),
+   * and then, the analysis results must be obtained again.
+   */
+  const moreResults = () => {
+    return dispatch => {
+      dispatch({
+        type: Actions.MoreResults
       });
+      return dispatch(getResults());
+    };
   };
+  return { getResults, moreResults };
 };
 
-const getAnalysisRequestedAction = () => {
-  return {
-    type: Actions.GetAnalysisRequested
-  };
-};
-
-const getAnalysisRejectedAction = () => {
-  return {
-    type: Actions.GetAnalysisRejected
-  };
-};
-
-const getAnalysisFulfilledAction = (analysis) => {
-  return {
-    type: Actions.GetAnalysisFulfilled,
-    analysis
-  };
-};
-
-export default getAnalysis;
+export default analysisDispatchers;
