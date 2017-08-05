63320093dc01a012bd4437cf4db8bd374ae4a77f
F. Javier R. Donado <fjrd84@gmail.com>
2017-08-05 13:23:34 +0200

    Analysis result element markup.

--- src/containers/home/components/home.js
+++ src/containers/home/components/home.js
@@ -1,7 +1,6 @@
 import React from 'react';
 import './home.css';
 
-
 const renderAnalysis = (data) => {
   if (typeof data.results === 'undefined') {
     return '';
@@ -9,7 +8,31 @@ const renderAnalysis = (data) => {
   const keys = Object.keys(data.results);
   let template = [];
   keys.forEach((key, idx) => {
-    template.push(<div key={'analysis-' + idx}>{data.results[key].analysis.problem}</div>);
+    template.push(
+      <li className="analysis-item" key={'analysis-' + idx}>
+        {data.results[key].analysis.problem}
+        <span class="date-and-query">
+          <i class="source" className="{data.results[key].source}"></i>
+          {data.results[key].source} - {data.results[key].created_at} <span hidden="data.results[key].query">- Query: "{data.results[key].query}"</span>
+        </span>
+        <div class="user-information">
+          <span class="name-and-description">
+            @{data.results[key].user_name}:
+            <span class="description" title="{data.results[key].user_description}">{data.results[key].user_description}</span>
+          </span>
+        </div>
+        <div class="message"
+          innerHTML="formatMessage(item)">
+          &ldquo;{data.results[key].message}&bdquo;
+        </div>
+        <div class="user-analysis">
+          <span className="{'positive': data.results[key].analysis.health_related}">
+            <span class="title">User Analysis: </span> {data.results[key].analysis.health_related ? data.results[key].analysis.profile
+          : 'not health related'}
+          </span>
+        </div>
+      </li>
+    );
   });
   return template;
 };
@@ -28,7 +51,9 @@ export class Home extends React.Component {
     return <div>
       <h1>Home</h1>
       <div id='analysis-section'>
-        {this.props.analysis ? renderAnalysis(this.props.analysis) : 'no-analysis'}
+        <ul id="analysis-list">
+          {this.props.analysis ? renderAnalysis(this.props.analysis) : 'no-analysis'}
+        </ul>
       </div>
       <p><button onClick={() => this.props.changePage()}>Go to about page via redux</button></p>
       <p><button onClick={() => {
