4a9513c756f78665840b84b52cfc84a3c1e03072
F. Javier R. Donado <fjrd84@gmail.com>
2017-08-11 21:38:39 +0200

    Refactor: analysis block as a component.

---
+++ src/containers/home/components/analysis-block.js
@@ -0,0 +1,29 @@
+import React from 'react';
+import MessageBlock from './message-block';
+
+export default class AnalysisBlock extends React.Component {
+
+  render() {
+
+    return <li className="analysis-item" key={'analysis-' + this.props.result.id}>
+      <span className="date-and-query">
+        <i className={'source ' + this.props.result.source}></i>
+        {this.props.result.source} - {this.props.result.created_at} <span hidden="this.props.result.query">- Query: "{this.props.result.query}"</span>
+      </span>
+      <div className="user-information">
+        <span className="name-and-description">
+          @{this.props.result.user_name}:
+            <span className="description" title={this.props.result.user_description}>{this.props.result.user_description}</span>
+        </span>
+      </div>
+      <MessageBlock item={this.props.result}></MessageBlock>
+      {/* &ldquo;{this.props.result.message}&bdquo; */}
+      <div className="user-analysis">
+        <span className={this.props.result.analysis.health_related ? 'positive' : ''}>
+          <span className="title">User Analysis: </span> {this.props.result.analysis.health_related ? this.props.result.analysis.profile
+            : 'not health related'}
+        </span>
+      </div>
+    </li>;
+  }
+}
--- src/containers/home/components/home.js
+++ src/containers/home/components/home.js
@@ -1,42 +1,7 @@
 import React from 'react';
 import Spinner from './spinner';
-import MessageBlock from './message-block';
+import AnalysisBlock from './analysis-block';
 import './home.css';
-
-
-
-const renderAnalysis = (data) => {
-  if (typeof data.results === 'undefined') {
-    return '';
-  }
-  const keys = Object.keys(data.results);
-  let template = [];
-  keys.forEach((key, idx) => {
-    template.push(
-      <li className="analysis-item" key={'analysis-' + data.results[key].id}>
-        <span className="date-and-query">
-          <i className={'source ' + data.results[key].source}></i>
-          {data.results[key].source} - {data.results[key].created_at} <span hidden="data.results[key].query">- Query: "{data.results[key].query}"</span>
-        </span>
-        <div className="user-information">
-          <span className="name-and-description">
-            @{data.results[key].user_name}:
-            <span className="description" title={data.results[key].user_description}>{data.results[key].user_description}</span>
-          </span>
-        </div>
-        <MessageBlock item={data.results[key]}></MessageBlock>
-        {/* &ldquo;{data.results[key].message}&bdquo; */}
-        <div className="user-analysis">
-          <span className={data.results[key].analysis.health_related ? 'positive' : ''}>
-            <span className="title">User Analysis: </span> {data.results[key].analysis.health_related ? data.results[key].analysis.profile
-              : 'not health related'}
-          </span>
-        </div>
-      </li>
-    );
-  });
-  return template;
-};
 
 export class Home extends React.Component {
 
@@ -48,7 +13,12 @@ export class Home extends React.Component {
     return <div>
       {this.props.analysis.results ? <div id='analysis-section'>
         <ul id="analysis-list">
-          {this.props.analysis ? renderAnalysis(this.props.analysis) : 'no-analysis'}
+          {this.props
+            .analysis
+            .results
+            .map(result =>
+              <AnalysisBlock key={'analysis-' + result.id} result={result}></AnalysisBlock>)
+          }
           {/* Show more button as last element of the list. */}
           <li className="more-results-btn"
             onClick={() => {
