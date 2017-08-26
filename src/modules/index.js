907e1b9e51d20298a5f1b582b7d5025530ea292d
F. Javier R. Donado <fjrd84@gmail.com>
2017-08-26 17:04:29 +0200

    Combine the showFormReducer.

--- src/modules/index.js
+++ src/modules/index.js
@@ -1,8 +1,9 @@
 import { combineReducers } from 'redux';
 import { routerReducer } from 'react-router-redux';
-import { analysisReducer } from './analysis/reducers';
+import { analysisReducer, showFormReducer } from './analysis/reducers';
 
 export default combineReducers({
   routing: routerReducer,
-  analysis: analysisReducer
+  analysis: analysisReducer,
+  form: showFormReducer
 });
