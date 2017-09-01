11cb8e48c163f2f02ef41bcabe3a8308163d3c21
F. Javier R. Donado <fjrd84@gmail.com>
2017-09-02 00:07:07 +0200

    Add Javier's description.

--- src/containers/about/components/about.js
+++ src/containers/about/components/about.js
@@ -23,11 +23,12 @@ const About = (props) => (
       Comments and suggestions are welcome. Contact us at info@lifescope-project.com</p>
     <h2>On the authors</h2>
     <p>
-      <a href="http://www.jdonado.com/">F. Javier R. Donado</a> is M.Eng. in Telecommunication and has been developing software professionally since 2010...
-    </p>
-    <p>
       <a href="https://github.com/JuanFF">Juan Fernández Fernández</a> holds a PhD in Natural Language Processing. He works in Analytics and Information Extraction systems based on insights into the properties of human language, specially Semantics and Cognition. He suffers from an autoimmune condition called alopecia areata, for which there is currently no effective treatment. He works in this project to make a contribution to research on hard to treat diseases.
     </p>
+    <p>
+      <a href="http://www.jdonado.com/">F. Javier R. Donado</a> holds a M.Eng. in Telecommunication and has been developing software professionally since 2010. He currently works as a software development team lead in Stuttgart (Germany). Javier has a wide experience in the development of modern web applications and is always looking for new challenges and interesting projects. He loves Open Source, VIM, functional programming and triple chocolate cookies.
+    </p>
+    <footer></footer>
   </div>
 );
 
