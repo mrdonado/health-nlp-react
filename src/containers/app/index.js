4fb9a75677b741b2c26612e3f79f5332c5e604c7
F. Javier R. Donado <fjrd84@gmail.com>
2017-09-06 21:44:53 +0200

    App as component. Side menu.

--- src/containers/app/index.js
+++ src/containers/app/index.js
@@ -5,30 +5,46 @@ import Home from '../home';
 import About from '../about/components/about';
 import './app.css';
 
+export default class App extends React.Component {
 
-const App = () => (
-  <div>
-    <nav>
-      <div id="burger-menu">
-        ≡
-      </div>
-      <div className="nav-content-wrapper">
-        <span className="logo">
-          lifescope
-      </span>
-        <NavLink to="/">
-          T/L
-      </NavLink>
-        <NavLink to="/about-us">
-          About
-      </NavLink>
-      </div>
-    </nav>
-    <main>
-      <Route exact path="/" component={Home} />
-      <Route exact path="/about-us" component={About} />
-    </main>
-  </div>
-);
+  componentDidMount = () => {
+    this.setState({ sideMenuActive: '' });
+  };
 
-export default App;
+  render() {
+    const navLinks = <div>
+      <NavLink to="/">T/L</NavLink>
+      <NavLink to="/about-us">About</NavLink>
+    </div>;
+
+    return <div>
+      <div id="side-menu"
+        className={this.state && this.state.sideMenuActive}>
+        <div id="close-menu"
+         onClick={() => {
+          this.setState({ sideMenuActive: '' });
+        }}>
+          X
+        </div>
+        {navLinks}
+      </div>
+      <nav>
+        <div id="burger-menu"
+          onClick={() => {
+            this.setState({ sideMenuActive: 'active' });
+          }}>
+          ≡
+      </div>
+        <div className="nav-content-wrapper">
+          <span className="logo">
+            lifescope</span>
+          {navLinks}
+        </div>
+      </nav>
+      <main>
+        <Route exact path="/" component={Home} />
+        <Route exact path="/about-us" component={About} />
+      </main>
+    </div>;
+  }
+}
