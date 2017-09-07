af60a67c455d51b29adb354c4bfadfb4a3ef499a
F. Javier R. Donado <fjrd84@gmail.com>
2017-09-07 22:36:01 +0200

    Home contents.

--- src/containers/home/components/home.js
+++ src/containers/home/components/home.js
@@ -1,9 +1,21 @@
 import React from 'react';
+import './home.css';
 
 export default class Home extends React.Component {
   render() {
-    return <div>
-      This is the home component.
+    return <div className="home-container">
+      <p>
+        Lifescope is an Artificial Intelligence algorithm that finds solutions to healthcare problems.
+      </p>
+      <p>
+        It's constantly monitoring social media feeds to search treatments for diseases.
+      </p>
+      <p>
+        It provides an accurate and real-time analysis of incoming messages in a timeline.
+      </p>
+      <p>
+        The algorithm analyzes not only the content, but also the sender of the message to exclude not health-related sources.
+      </p>
     </div>
   }
 }
