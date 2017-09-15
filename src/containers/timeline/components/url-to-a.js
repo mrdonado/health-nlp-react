abe609bec9a90a34a4a06251bf0d6838d78020de
F. Javier R. Donado <fjrd84@gmail.com>
2017-09-15 21:35:09 +0200

    Simplify url search.

--- src/containers/timeline/components/url-to-a.js
+++ src/containers/timeline/components/url-to-a.js
@@ -1,16 +1,19 @@
 import React from 'react';
 
 const UrlToA = (props) => {
-  let regex = /(https?:\/\/)?([\da-z.-]+)\.([a-z.]{2,6})([/\w .-]*)*\/?/;
-  let match = props.text.match(regex);
-  let div = props.text;
+  let regex = /(https?:\/\/)/;
+  let div = props.text.replace(/[\n\r\s\t]/g, ' ');
+  let match = div.match(regex);
   if (match) {
-    div = div.split(match[0].split(' ')[0]).map((t,idx) => {
-      if (t === "") {
-        return <a key={match[0] + idx} href={match[0]}>{match[0]}</a>;
-      }
-      return t;
-    });
+    let from = div.indexOf(match[0]);
+    let to = div.indexOf(' ', from);
+    to = to > -1 ? to : div.length;
+    let url = div.substring(from, to);
+    div = [div.substring(0, from),
+    <a key={url} target="blank" href={url}>{url}</a>];
+    if (to < props.text.length) {
+      div.push(' ' + props.text.substring(to + 1));
+    }
   }
   return <i>{div}</i>;
 };
