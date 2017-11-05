ea9dc82dc186d0cd7edf0dc96a663de737204758
F. Javier R. Donado <fjrd84@gmail.com>
2017-11-05 13:14:48 +0100

    Show the more button only if the list is not empty.

--- src/containers/timeline/components/analysis-list.js
+++ src/containers/timeline/components/analysis-list.js
@@ -4,7 +4,7 @@ import AnalysisBlock from './analysis-block';
 export default class AnalysisList extends React.Component {
   render() {
 
-    let list = (this.props.analysis && 
+    let list = (this.props.analysis &&
       this.props.analysis.results) || [];
 
     return <ul id="analysis-list">
@@ -12,14 +12,15 @@ export default class AnalysisList extends React.Component {
         .map(result =>
           <AnalysisBlock key={'analysis-' + result.id} result={result}></AnalysisBlock>)
       }
-
       {/* Show more button as last element of the list. */}
-      <button className="more-results-btn"
-        onClick={() => {
-          this.props.moreResults();
-        }}>
-        Show more
+      {list.length > 0 ?
+        <button className="more-results-btn"
+          onClick={() => {
+            this.props.moreResults();
+          }}>
+          Show more
           </button>
+        : ''}
     </ul>;
   }
 }
