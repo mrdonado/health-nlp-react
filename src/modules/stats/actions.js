b2fff16bb778d726f2ae8d0398ff20957a48dce9
F. Javier R. Donado <fjrd84@gmail.com>
2017-11-06 22:04:44 +0100

    Add stats actions list.

---
+++ src/modules/stats/actions.js
@@ -0,0 +1,15 @@
+// ------------------------------------
+// Constants
+// ------------------------------------
+const Actions = {
+  GetProblemsList: 'GET_PROBLEMS_LIST',
+  GetProfilesList: 'GET_PROFILES_LIST',
+  GetQueriesList: 'GET_QUERIES_LIST',
+  GetSolutionsToProblem: 'GET_SOLUTIONS_TO_PROBLEM',
+  SearchMessages: 'SEARCH_MESSAGES',
+  GetHistogramDays: 'GET_HISTOGRAM_DAYS',
+  GetHistogramWeeks: 'GET_HISTOGRAM_WEEKS',
+  GetHistogramMonths: 'GET_HISTOGRAM_MONTHS'
+};
+
+export default Actions;
