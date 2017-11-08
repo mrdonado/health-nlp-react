9258d4427a79d655c072d6b59e4bd43a0d1f7a65
F. Javier R. Donado <fjrd84@gmail.com>
2017-11-08 21:16:09 +0100

    Combine reducers with the stats reducer.

--- src/modules/index.js
+++ src/modules/index.js
@@ -2,10 +2,12 @@ import { combineReducers } from 'redux';
 import { routerReducer } from 'react-router-redux';
 import { reducer as reduxFormReducer } from 'redux-form';
 import { analysisReducer, swapFormReducer } from './analysis/reducers';
+import { statsReducer } from './stats/reducers';
 
 export default combineReducers({
   routing: routerReducer,
   analysis: analysisReducer,
+  stats: statsReducer,
   form: reduxFormReducer,
   formWindow: swapFormReducer
 });
