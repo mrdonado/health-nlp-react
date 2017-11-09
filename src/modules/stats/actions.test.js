9d341178f946a83a535d603592c461364401eb89
F. Javier R. Donado <fjrd84@gmail.com>
2017-11-09 22:05:06 +0100

    Action set messages count.

--- src/modules/stats/actions.test.js
+++ src/modules/stats/actions.test.js
@@ -5,7 +5,7 @@ describe('Analysis actions', () => {
     expect(Actions.GetHistogramDays).toBeDefined();
     expect(Actions.GetHistogramMonths).toBeDefined();
     expect(Actions.GetHistogramWeeks).toBeDefined();
-    expect(Actions.GetMessagesCount).toBeDefined();
+    expect(Actions.SetMessagesCount).toBeDefined();
     expect(Actions.GetProblemsList).toBeDefined();
     expect(Actions.GetProfilesList).toBeDefined();
     expect(Actions.GetQueriesList).toBeDefined();
