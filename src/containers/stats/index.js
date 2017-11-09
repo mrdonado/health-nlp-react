9afbf233466a4814de853f7018cb9a8611ef8a12
F. Javier R. Donado <fjrd84@gmail.com>
2017-11-09 22:04:06 +0100

    Bind fetchMessagesCount to the stats properties.

--- src/containers/stats/index.js
+++ src/containers/stats/index.js
@@ -1,13 +1,17 @@
 // import { bindActionCreators } from 'redux';
 import { connect } from 'react-redux';
+import { bindActionCreators } from 'redux';
 import Stats from './components/stats';
+import { fetchMessagesCount } from '../../modules/stats/dispatchers';
 
 const mapStateToProps = state => ({
   stats: state.stats
 });
 
 const mapDispatchToProps = dispatch => {
-  return {};
+  return bindActionCreators({
+    fetchMessagesCount
+  }, dispatch);
 };
 
 export default connect(
