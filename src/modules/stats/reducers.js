997c4a4a06ecf28641ab30b4e520b81a7c99470a
F. Javier R. Donado <fjrd84@gmail.com>
2017-11-11 17:31:10 +0100

    Set the messages count.

--- src/modules/stats/reducers.js
+++ src/modules/stats/reducers.js
@@ -2,6 +2,9 @@ import Actions from './actions';
 
 const statsReducer = (stats = {}, action) => {
   switch (action.type) {
+    case Actions.SetMessagesCount:
+      return Object.assign({}, stats, { count: action.count });
+      break;
     default:
       return stats;
   }
