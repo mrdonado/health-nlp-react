e0bfe886aabff5378cc9182a401b2abbac56ac3a
F. Javier R. Donado <fjrd84@gmail.com>
2017-11-12 21:36:40 +0100

    Test fetch messages count.

--- src/modules/stats/dispatchers.test.js
+++ src/modules/stats/dispatchers.test.js
@@ -1,11 +1,31 @@
-import { setMessagesCount } from './dispatchers';
+import {
+  setMessagesCount,
+  fetchMessagesCount
+} from './dispatchers';
 import Actions from './actions';
+import fetchMock from 'fetch-mock';
 
+beforeAll(() => {
+  fetchMock.get('*', { 'messages-count': 55 });
+});
+
+afterAll(() => {
+  fetchMock.restore();
+});
 
 describe('Stats action dispatchers', () => {
+
   it('should dispatch the set messages count action', () => {
     const action = setMessagesCount(5);
     expect(action.type).toEqual(Actions.SetMessagesCount);
     expect(action.count).toEqual(5);
+  });
+
+  it('should fetch the current messages count', (done) => {
+    fetchMessagesCount()((action) => {
+      expect(action.type).toEqual("SET_MESSAGES_COUNT");
+      expect(action.count).toEqual(55);
+      done();
+    });
   });
 });
