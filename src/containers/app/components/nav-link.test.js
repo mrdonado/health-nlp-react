46c32b426b7692c3fc1f26679f4b6fc81e5edad4
F. Javier R. Donado <fjrd84@gmail.com>
2017-11-15 20:34:19 +0100

    NavLink test.

---
+++ src/containers/app/components/nav-link.test.js
@@ -0,0 +1,37 @@
+import React from 'react';
+import NavLink from './nav-link';
+import ReactDOM from 'react-dom';
+import { shallow } from 'enzyme';
+import { StaticRouter } from 'react-router-dom';
+
+describe('NavLink component', () => {
+
+  let _props, _spies, _wrapper, _context;
+
+  beforeEach(() => {
+    _spies = {};
+    _props = { children: '', to: '' };
+    _context = {
+      router: { route: { location: { pathname: 'example' } } }
+    };
+    _wrapper = shallow(<NavLink {..._props} />, { context: _context });
+  });
+
+  it('renders without crashing', () => {
+    const div = document.createElement('div');
+    const context = {};
+    ReactDOM.render(
+      <StaticRouter location="someLocation" context={context}>
+        <NavLink to="'example'" />
+      </StaticRouter>
+      , div);
+  });
+
+  it('assigns the active class for links pointing to the current path name', () => {
+    _wrapper.setProps({ children: '', to: '' });
+    expect(_wrapper.find('.active').length).toEqual(0);
+    _wrapper.setProps({ children: '', to: 'example' });
+    expect(_wrapper.find('.active').length).toEqual(1);
+  });
+
+});
