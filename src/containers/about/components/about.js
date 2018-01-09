e726a8e7cd12253d1d694428f4c7e3b2730dcfd2
F. Javier R. Donado <fjrd84@gmail.com>
2018-01-09 22:11:03 +0100

    Modify text and insert photos.

--- src/containers/about/components/about.js
+++ src/containers/about/components/about.js
@@ -1,5 +1,7 @@
 import React from 'react';
 import './about.css';
+import juanPhoto from '../../../assets/images/juan-photo.jpg';
+import javiPhoto from '../../../assets/images/javi-photo.jpg';
 
 export default class About extends React.Component {
 
@@ -10,6 +12,7 @@ export default class About extends React.Component {
 
   render() {
     return <div id="about-content">
+      <h2>About the lifescope project</h2>
       <p>
         Lifescope is an Artificial Intelligence software that <strong>finds solutions to health diseases</strong> in digital sources, such as social networks or news feeds. It consists of a live stream of text messages, and a Natural Language Processing algorithm that extracts health problems and solutions, and provides semantic labels to categorize the message sender profile. In other words, it finds out <strong>what the message says and who says it</strong>, providing an accurate and real-time analysis on massive health data.
     </p>
@@ -19,23 +22,32 @@ export default class About extends React.Component {
       <p>
         We believe that Lifescope will offer <strong>great research opportunities to anyone interested in healthcare information</strong>. We are concerned with the challenging issues posed by the New Media Age, where information flow surpasses the human ability to digest new data - consider, for example, how many talks, conferences or health discoveries are being posted now, at any second. That’s where Lifescope comes in.
     </p>
-      <p>
+      {/* <p>
         Currently, the project is on development. After this first stage, <strong>we have completed the backend, frontend and analytics modules</strong> of the streaming panel that you can see in this web. We are working on <strong>a new module to make queries</strong> and <strong>display graphs</strong> on the dataset we are gathering. In the meantime, you can see an example of <a href="http://insights.lifescope-project.com/2017/09/24/202/">some raw data files here</a>.
+    </p> */}
+      <p>
+        Please, <strong>do not take the information provided here as medical advice</strong>.
+        {/* On the other hand, although we are constantly fine-tuning the algorithm, you may find errors in some analysis outputs - for example, a word like “hospital” wrongly annotated as a treatment.  */} Every artificial intelligence software is prone to errors, as they never substitute humans. In any case, the <strong><a target="_blank" rel="noopener noreferrer" href="http://insights.lifescope-project.com/2017/09/24/algorithms-confidence-rate-79-42/">current performance score</a> is around 80%</strong>.
     </p>
       <p>
-        Please, <strong>do not take the information provided here as medical advice</strong>. On the other hand, although we are constantly fine-tuning the algorithm, you may find errors in some analysis outputs - for example, a word like “hospital” wrongly annotated as a treatment. Every artificial intelligence software is prone to errors, as they never substitute humans. In any case, the <strong><a href="http://insights.lifescope-project.com/2017/09/24/algorithms-confidence-rate-79-42/">current performance score</a> is around 80%</strong>.
-    </p>
+        <strong>Visit the <a target="_blank" rel="noopener noreferrer" href="http://insights.lifescope-project.com">Lifescope insights</a> blog</strong> to get more detailed information about the data being processed and analyzed within this project. You can also find a detailed <strong>technical documentation about its architecture</strong> in <a target="_blank" rel="noopener noreferrer" href="https://blog.jdonado.com/lifescope-project/">JDonado's Readme</a>.
+        </p>
       <p>
-        Comments and suggestions are welcome. Contact us at <a href="mailto:info@lifescope-project.com">info@lifescope-project.com</a>
+        Comments and suggestions are welcome. Contact us at < a href="mailto:info@lifescope-project.com" > info@lifescope-project.com</a >
       </p>
       <h2>On the authors</h2>
-      <p>
-        <a href="https://www.linkedin.com/in/juanff/">Juan Fernández Fernández</a> holds a PhD in Natural Language Processing. He works in Analytics and Information Extraction systems based on insights into the properties of human language, specially Semantics and Cognition. He suffers from an autoimmune condition called alopecia areata, for which there is currently no effective treatment. He works in this project to make a contribution to research on hard to treat diseases.
-    </p>
-      <p>
-        <a href="http://www.jdonado.com/">F. Javier R. Donado</a> holds a M.Eng. in Telecommunication and has been developing software professionally since 2010. He currently works as a software development team lead in Stuttgart (Germany). Javier has a wide experience in the development of modern web applications and is always looking for new challenges and interesting projects. He loves Open Source, VIM, functional programming and triple chocolate cookies.
-    </p>
+      <p className="author-data">
+        <p className="left-paragraph">
+        <a target="_blank" rel="noopener noreferrer" href="https://www.linkedin.com/in/juanff/">Juan Fernández Fernández</a> holds a PhD in Natural Language Processing. He works in Analytics and Information Extraction systems based on insights into the properties of human language, specially Semantics and Cognition. He suffers from an autoimmune condition called alopecia areata, for which there is currently no effective treatment. He works in this project to make a contribution to research on hard to treat diseases.
+        </p>
+        <img className="right-photo" src={juanPhoto}></img>
+      </p>
+      <p className="author-data">
+        <img className="left-photo" src={javiPhoto}></img>
+        <p className="right-paragraph">
+          <a target="_blank" rel="noopener noreferrer" href="http://www.jdonado.com/">F. Javier R. Donado</a> holds a M.Eng. in Telecom and has been developing software professionally since 2010. He currently works as a software development team lead in Stuttgart (Germany). Javier has a wide experience in the development of modern web applications and is always looking for new challenges and interesting projects. He loves Open Source, VIM, functional programming and triple chocolate cookies.</p>
+      </p>
       <footer></footer>
-    </div>
+    </div >
   }
 }
