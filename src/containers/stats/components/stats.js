4e5acfce3ae449575bc550a7a323c15feaa81a9c
F. Javier R. Donado <fjrd84@gmail.com>
2018-01-25 20:07:35 +0100

    Integrate stats with the stats component.

--- src/containers/stats/components/stats.js
+++ src/containers/stats/components/stats.js
@@ -14,12 +14,26 @@ export default class Home extends React.Component {
     return <div className="main-content">
       <div className="left-panel">
         SOME STATS {this.props.stats.count}
-        </div>
+        <input id="free-text" type="text" />
+        <input onClick={() => this.props.fetchWordSearch(document.getElementById('free-text').value)}
+          type="button"
+          value="Search" />
+        <select onChange={(v) => this.props.fetchSolutionsToProblem(v.target.value)}>
+          <option>-select problem-</option>
+          {(this.props.stats.problems || [])
+            .map(p => <option key={p.key} value={p.key}>{p.key}</option>)}
+        </select>
+        <select onChange={(v) => this.props.fetchMessagesForProblemSolution(this.props.stats.problem, v.target.value)}>
+          <option>-select solution-</option>
+          {(this.props.stats.solutions || [])
+            .map(p => <option key={p.key} value={p.key}>{p.key}</option>)}
+        </select>
+      </div>
       <div className="right-panel">
-        <AnalysisList analysis={analysisList}
+        <AnalysisList analysis={{ results: this.props.stats.messages || [] }}
           moreResults={() => { }}>
         </AnalysisList>
       </div>
-    </div>
+    </div >
   }
 };
