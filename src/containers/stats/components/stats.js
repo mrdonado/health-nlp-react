dfc808a60b864b55facf4b25a78c40202a6161ad
F. Javier R. Donado <fjrd84@gmail.com>
2018-07-29 17:49:14 +0200

    Slider selects free search vs. problem/solution

--- src/containers/stats/components/stats.js
+++ src/containers/stats/components/stats.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import './stats.css';
+import './slider.css';
 import AnalysisList from '../../timeline/components/analysis-list';
 import Spinner from '../../../utilities/spinner';
 
@@ -7,7 +8,11 @@ export default class Home extends React.Component {
 
   constructor(props) {
     super(props);
-    this.state = { resultsDisplayed: 5, searchString: '' };
+    this.state = {
+      resultsDisplayed: 5,
+      searchString: '',
+      freeSearch: false
+    };
   }
 
   componentDidMount() {
@@ -26,53 +31,82 @@ export default class Home extends React.Component {
     }
     return <div className="main-content stats-grid">
       <div className="left-panel">
+        <div>
+          <div className="switch-selector">
+            <span className={
+              !this.state.freeSearch && 'active'
+            }>Problem/Solution</span>
+            <label className="switch">
+              <input type="checkbox"
+                id="free-search"
+                onChange={(e) => {
+                  this.setState({
+                    freeSearch: e.target.checked
+                  });
+                }}
+                iname="free-search"
+                label="Free search" />
+              <span className="slider" />
+            </label>
+            <span className={
+              this.state.freeSearch && 'active'
+            }>Free search</span>
+          </div>
+        </div>
         <div className="data-box">
           <div className="box-title">
             Total number of analyzed messages
           </div>
           {this.props.stats.count}
         </div>
-        <input id="free-text" type="text"
-          onChange={v =>
-            this.setState({
-              searchString: v.target.value
-            })}
-        />
-
-        <input
-          id="search-button"
-          onClick={() => {
-            this.props.fetchWordSearch(this.state.searchString);
-          }}
-          type="button"
-          value="Search" />
-        <select
-          id="problem-select"
-          onChange={v => this.props.fetchSolutionsToProblem(v.target.value)}>
-          <option>-select problem-</option>
-          {(this.props.stats.problems || [])
-            .map(p => <option key={p.key} value={p.key}>{p.key}</option>)}
-        </select>
-        <select
-          id="solution-select"
-          disabled={!Array.isArray(this.props.stats.solutions)}
-          onChange={(v) => {
-            this
-              .props
-              .fetchMessagesForProblemSolution(this.props.stats.problem, v.target.value);
-          }}>
-          <option>-select solution-</option>
-          {(this.props.stats.solutions || [])
-            .map(p => <option key={p.key} value={p.key}>{p.key}</option>)}
-        </select>
         <div className="data-box">
           <div className="box-title">
             Current search results
           </div>
           {(this.props.stats.messages || []).length}
         </div>
+
+        {this.state.freeSearch ?
+          <div>
+            <input id="free-text" type="text"
+              onChange={v =>
+                this.setState({
+                  searchString: v.target.value
+                })} />
+            <input
+              id="search-button"
+              onClick={() => {
+                this.props.fetchWordSearch(this.state.searchString);
+              }}
+              type="button"
+              value="Search" />
+          </div>
+          :
+          <div>
+            <select
+              id="problem-select"
+              onChange={v => this.props.fetchSolutionsToProblem(v.target.value)}>
+              <option>-select problem-</option>
+              {(this.props.stats.problems || [])
+                .map(p => <option key={p.key} value={p.key}>{p.key}</option>)}
+            </select>
+            <select
+              id="solution-select"
+              disabled={!Array.isArray(this.props.stats.solutions)}
+              onChange={(v) => {
+                this
+                  .props
+                  .fetchMessagesForProblemSolution(this.props.stats.problem, v.target.value);
+              }}>
+              <option>-select solution-</option>
+              {(this.props.stats.solutions || [])
+                .map(p => <option key={p.key} value={p.key}>{p.key}</option>)}
+            </select>
+          </div>
+        }
       </div>
       <div className="right-panel">
+
         <AnalysisList analysis={{
           results: (this.props.stats.messages || [])
             .slice(0, this.state.resultsDisplayed)
