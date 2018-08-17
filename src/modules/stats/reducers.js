a45985f6721037e05969f996184562d9cd33bd58
F. Javier R. Donado <fjrd84@gmail.com>
2018-08-17 20:16:53 +0200

    Filter irrelevant keywords

--- src/modules/stats/reducers.js
+++ src/modules/stats/reducers.js
@@ -1,4 +1,20 @@
 import Actions from './actions';
+const IRRELEVANT_KEYWORDS = [
+  'a',
+  'the',
+  'this',
+  'these',
+  'other',
+  'others'
+];
+const keyWordsFilter = (list) => {
+  return list
+    .filter(e => {
+      console.log('.');
+      return (e.key.length > 2) &&
+        (IRRELEVANT_KEYWORDS.indexOf(e.key) === -1);
+    });
+};
 
 const statsReducer = (stats = {}, action) => {
   let pendingRequests = [...(stats.pendingRequests || [])];
@@ -6,11 +22,15 @@ const statsReducer = (stats = {}, action) => {
     case Actions.SetMessagesCount:
       return Object.assign({}, stats, { count: action.count });
     case Actions.SetProblemsList:
-      return Object.assign({}, stats, { problems: action.problems });
+      return Object.assign({}, stats, {
+        problems: keyWordsFilter(action.problems)
+      });
     case Actions.IncrementMessagesCount:
       return Object.assign({}, stats, { count: stats.count + 1 });
     case Actions.SetSolutionsList:
-      return Object.assign({}, stats, { solutions: action.solutions });
+      return Object.assign({}, stats, {
+        solutions: keyWordsFilter(action.solutions)
+      });
     case Actions.SetMessagesList:
       return Object.assign({}, stats, { messages: action.messages });
     case Actions.SetProblem:
