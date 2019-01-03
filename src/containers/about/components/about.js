4ed3a2cc0918ee91548f68836efb0a7421a18957
Juan Fernández <jnferfer1@gmail.com>
2019-01-03 20:13:59 +0100

    Modify About text

--- src/containers/about/components/about.js
+++ src/containers/about/components/about.js
@@ -15,33 +15,29 @@ export default class About extends React.Component {
     return <div id="about-content">
       <h2>About lifescope</h2>
       <p>
-        Lifescope is an Artificial Intelligence software that <strong>finds solutions to health diseases</strong> in digital sources, such as social networks or news feeds. It consists of a live stream of text messages, and a Natural Language Processing algorithm that extracts health problems and solutions, and provides semantic labels to categorize the message sender profile. In other words, it finds out <strong>what the message says and who says it</strong>, providing an accurate and real-time analysis on massive health data.
+        Lifescope is an Artificial Intelligence software that <strong>finds treatments for health diseases</strong> in digital sources, such as social networks, news feeds or medical databases. It consists of a live stream of text messages, and a Natural Language Processing algorithm that extracts mentions to health diseases and treatments, and provides semantic labels to categorize the message sender profile. In other words, it finds out <strong>what the message says and who says it</strong>, providing an accurate and real-time analysis on massive health data.
     </p>
       <p>
-        This is an <a target="_blank" rel="noopener noreferrer" href="https://github.com/fjrd84/health-nlp-analysis"> open source</a> software project. It aims to provide the latest information on healthcare, and helps in clarifying it by <strong>making the sender explicit </strong>and discarding unknown sources.
+        This is an <a target="_blank" rel="noopener noreferrer" href="https://github.com/fjrd84/health-nlp-analysis"> open source</a> software project. It aims to provide the latest information on healthcare, and helps in clarifying it by making the sender explicit and discarding unknown sources.
     </p>
       <p>
-        We believe that Lifescope will offer <strong>great research opportunities to anyone interested in healthcare information</strong>. We are concerned with the challenging issues posed by the New Media Age, where information flow surpasses the human ability to digest new data - consider, for example, how many talks, conferences or health discoveries are being posted now, at any second. That’s where Lifescope comes in.
+        We believe that Lifescope will offer great <strong>research opportunities</strong> to anyone interested in healthcare information. We are concerned with the challenging issues posed by the New Media Age, where information flow surpasses the human ability to digest new data - consider, for example, how many talks, conferences or health discoveries are being posted now, at any second.
     </p>
-      {/* <p>
-        Currently, the project is on development. After this first stage, <strong>we have completed the backend, frontend and analytics modules</strong> of the streaming panel that you can see in this web. We are working on <strong>a new module to make queries</strong> and <strong>display graphs</strong> on the dataset we are gathering. In the meantime, you can see an example of <a href="http://insights.lifescope-project.com/2017/09/24/202/">some raw data files here</a>.
-    </p> */}
+      <p>
+        This project is <strong>on development</strong>. We have finished the backend and text analysis modules of the streaming panel that you can see in this web. Contact us at < a href="mailto:info@lifescope-project.com" > info@lifescope-project.com</a > if you wish to access the data we are gathering from May 2018.
+    </p>
 
       <p>
         <img className="right-photo" alt="Lifescope Insights" src={lifescopeInsightsPhoto}></img>
-        <strong>Visit the <a target="_blank" rel="noopener noreferrer" href="http://insights.lifescope-project.com">Lifescope insights</a> blog</strong> to get more detailed information about the data being processed and analyzed within this project. You can also find a detailed <strong>technical documentation about its architecture</strong> in <a target="_blank" rel="noopener noreferrer" href="https://blog.jdonado.com/lifescope-project/">JDonado's Readme</a>.
+        Visit <a target="_blank" rel="noopener noreferrer" href="http://insights.lifescope-project.com">this page</a> to get detailed information about the data sources and confidence of the algorithm. You can also find detailed technical documentation about the backend architecture in <a target="_blank" rel="noopener noreferrer" href="https://blog.jdonado.com/lifescope-project/">JDonado's Readme</a>.
         </p>
       <p>
         Please, <strong>do not take the information provided here as medical advice</strong>.
-        {/* On the other hand, although we are constantly fine-tuning the algorithm, you may find errors in some analysis outputs - for example, a word like “hospital” wrongly annotated as a treatment.  */} Every artificial intelligence software is prone to errors, as they never substitute humans. In any case, the <strong><a target="_blank" rel="noopener noreferrer" href="http://insights.lifescope-project.com/2017/09/24/algorithms-confidence-rate-79-42/">current performance score</a> is around 80%</strong>.
     </p>
-      <p>
-        Comments and suggestions are welcome. Contact us at < a href="mailto:info@lifescope-project.com" > info@lifescope-project.com</a >
-      </p>
       <h2>On the authors</h2>
       <p className="author-info">
         <img className="right-photo" alt="Juan Fernández" src={juanPhoto}></img>
-        <a target="_blank" rel="noopener noreferrer" href="https://www.linkedin.com/in/juanff/">Juan Fernández Fernández</a> holds a PhD in Natural Language Processing. He works in Analytics and Information Extraction systems based on insights into the properties of human language, specially Semantics and Cognition. He suffers from an autoimmune condition called alopecia areata, for which there is currently no effective treatment. He works in this project to make a contribution to research on hard to treat diseases.
+        <a target="_blank" rel="noopener noreferrer" href="https://www.linkedin.com/in/juanff/">Juan Fernández Fernández</a> holds a PhD in Natural Language Processing. He works in Information Extraction and Dialogue systems based on insights into the properties of human language, specially Semantics and Cognition. He suffers from an autoimmune condition called alopecia areata, for which there is currently no effective treatment. He works in this project to make a contribution to research on hard to treat diseases.
         </p>
       <p className="author-info">
         <img className="left-photo" alt="F. Javier R. Donado" src={javiPhoto}></img>
